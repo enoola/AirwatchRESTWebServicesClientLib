c24c9298ba7eb92984a3e70b253b30f514fa5b0a
John <john@mobilutils.com>
2018-02-14 13:54:12 +0000

    started to add CLASS_SENTENCE_AIM const

--- src/PhPeteur/AirwatchWebservices/Exception/AirwatchCmdException.php
+++ src/PhPeteur/AirwatchWebservices/Exception/AirwatchCmdException.php
@@ -11,7 +11,6 @@ namespace PhPeteur\AirwatchWebservices\Exception;
 
 class AirwatchCmdException extends \Exception
 {
-
     // Redefine the exception so message isn't optional
     public function __construct($message, $code = 0, Exception $previous = null) {
         // some code
